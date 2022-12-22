e5710e82e4cccde1c2e3a426e7fb1f987b241fc4
איתי בר-חן <u9282364@bsmch.net>
2022-12-22 13:52:16 +0200

    tttt

--- OneJS/GameUI/components/BottomDrawer.js
+++ OneJS/GameUI/components/BottomDrawer.js
@@ -2,28 +2,37 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.BottomDrawer = void 0;
 var preact_1 = require("preact");
 var hooks_1 = require("preact/hooks");
+var hooks_2 = require("preact/hooks");
 var UnityEngine_1 = require("UnityEngine");
+var UIElements_1 = require("UnityEngine/UIElements");
 var BottomDrawer = function () {
-    var _a = (0, hooks_1.useState)(false), expanded = _a[0], setExpanded = _a[1];
-    var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 5, 6, 7, 8, 9, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 5, 6, 7, 8, 9, 30];
+    var _a = (0, hooks_2.useState)(false), expanded = _a[0], setExpanded = _a[1];
+    var data = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 5, 6, 7, 8, 9, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        4, 5, 6, 7, 8, 9, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 5, 6, 7, 8, 9, 30, 1, 2,
+        3, 4, 5, 6, 7, 8, 9, 4, 5, 6, 7, 8, 9, 30,
+    ];
     var RotateScript = UnityEngine_1.GameObject.Find("Main Camera");
     var setCanScroll = function (bool) {
         RotateScript.GetComponents(UnityEngine_1.Component)[3].canRotate = bool;
     };
-    return ((0, preact_1.h)("div", { style: { height: expanded ? 200 : 36 }, class: "w-full absolute bottom-0" },
+    (0, hooks_1.useEffect)(function () {
+        setCanScroll(!expanded);
+    }, [expanded]);
+    return ((0, preact_1.h)("div", { style: { height: expanded ? 200 : 36 }, class: "w-full absolute bottom-0 transition-[all] ease-out duration-[0.25]" },
         (0, preact_1.h)("div", { onClick: function () { return setExpanded(!expanded); }, class: "absolute -top-5 w-full" },
-            (0, preact_1.h)("div", { style: { height: 14, width: 74 }, class: "rounded-lg bg-white mx-auto" },
-                (0, preact_1.h)("textfield", null))),
+            (0, preact_1.h)("div", { style: { height: 14, width: 74 }, class: "rounded-lg bg-white mx-auto opacity-80" },
+                (0, preact_1.h)("div", { class: "text-gray-400 font-thin mt-[2px] ml-1", style: { fontSize: 8 } }, "Search..."))),
         (0, preact_1.h)("div", { class: "bg-white h-full rounded-t-lg opacity-40" }),
         (0, preact_1.h)("div", { class: "absolute top-0 w-full h-full" },
-            (0, preact_1.h)("scrollview", null,
+            (0, preact_1.h)("scrollview", { "vertical-scroller-visibility": UIElements_1.ScrollerVisibility.Hidden },
                 (0, preact_1.h)("div", { class: "flex-row justify-center pt-1 flex-wrap" }, data.map(function (num, index) {
                     if (expanded) {
-                        return (0, preact_1.h)("div", { class: "w-7 h-7 rounded-lg bg-gray-500 mx-1 mb-2", onClick: function () { return setExpanded(false); } });
+                        return ((0, preact_1.h)("div", { class: "w-7 h-7 rounded-lg bg-gray-500 mx-1 mb-2", onClick: function () { return setExpanded(false); } }));
                     }
                     else {
                         if (index < 4) {
-                            return (0, preact_1.h)("div", { class: "w-7 h-7 rounded-lg bg-gray-500 mx-1", onClick: function () { return setExpanded(false); } });
+                            return ((0, preact_1.h)("div", { class: "w-7 h-7 rounded-lg bg-gray-500 mx-1", onClick: function () { return setExpanded(false); } }));
                         }
                     }
                 }))))));
--- OneJS/GameUI/components/BottomDrawer.tsx
+++ OneJS/GameUI/components/BottomDrawer.tsx
@@ -1,40 +1,80 @@
-import { h } from "preact"
-import { useState } from "preact/hooks"
-import { Component, GameObject } from "UnityEngine";
+import { h } from "preact";
+import { useEffect } from "preact/hooks";
+import { useState } from "preact/hooks";
+import {
+  Component,
+  GameObject,
+  MeshRenderer,
+  PrimitiveType,
+} from "UnityEngine";
+import { ScrollerVisibility, TextInputBaseField } from "UnityEngine/UIElements";
 
 export const BottomDrawer = () => {
-    const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
-    let data = [1,2,3,4,5,6,7,8,9,4,5,6,7,8,9,30,1,2,3,4,5,6,7,8,9,4,5,6,7,8,9,30];
+  let data = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 5, 6, 7, 8, 9, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    4, 5, 6, 7, 8, 9, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 5, 6, 7, 8, 9, 30, 1, 2,
+    3, 4, 5, 6, 7, 8, 9, 4, 5, 6, 7, 8, 9, 30,
+  ];
 
-    var RotateScript = GameObject.Find("Main Camera");
-    
-    const setCanScroll = (bool) => {
-        RotateScript.GetComponents(Component)[3].canRotate = bool;
-    }
+  var RotateScript = GameObject.Find("Main Camera");
 
-    return (
+  const setCanScroll = (bool) => {
+    RotateScript.GetComponents(Component)[3].canRotate = bool;
+  };
 
-        <div style={{height: expanded ? 200 : 36}}  class="w-full absolute bottom-0">
-        <div onClick={() => setExpanded(!expanded)} class="absolute -top-5 w-full"><div style={{height: 14, width: 74}} class="rounded-lg bg-white mx-auto"><textfield></textfield></div></div>
-        <div class="bg-white h-full rounded-t-lg opacity-40"></div>
-        <div class="absolute top-0 w-full h-full">
-          
-          <scrollview>
-            <div class="flex-row justify-center pt-1 flex-wrap">
-                {data.map((num, index) => {
-                    if (expanded){
-                        return <div class="w-7 h-7 rounded-lg bg-gray-500 mx-1 mb-2" onClick={() => setExpanded(false)}></div>
-                    } else {
-                        if (index < 4){
-                            return <div class="w-7 h-7 rounded-lg bg-gray-500 mx-1" onClick={() => setExpanded(false)}></div>
-                        }
-                    }
-                })}
-            </div>
-            </scrollview>
+  useEffect(() => {
+    setCanScroll(!expanded);
+  }, [expanded]);
+
+  return (
+    <div
+      style={{ height: expanded ? 200 : 36 }}
+      class="w-full absolute bottom-0 transition-[all] ease-out duration-[0.25]"
+    >
+      <div
+        onClick={() => setExpanded(!expanded)}
+        class="absolute -top-5 w-full"
+      >
+        <div
+          style={{ height: 14, width: 74 }}
+          class="rounded-lg bg-white mx-auto opacity-80"
+        >
+          <div
+            class="text-gray-400 font-thin mt-[2px] ml-1"
+            style={{ fontSize: 8 }}
+          >
+            Search...
+          </div>
         </div>
-        </div>
-      
-    )
-}
+      </div>
+      <div class="bg-white h-full rounded-t-lg opacity-40"></div>
+      <div class="absolute top-0 w-full h-full">
+        <scrollview vertical-scroller-visibility={ScrollerVisibility.Hidden}>
+          <div class="flex-row justify-center pt-1 flex-wrap">
+            {data.map((num, index) => {
+              if (expanded) {
+                return (
+                  <div
+                    class="w-7 h-7 rounded-lg bg-gray-500 mx-1 mb-2"
+                    onClick={() => setExpanded(false)}
+                  ></div>
+                );
+              } else {
+                if (index < 4) {
+                  return (
+                    <div
+                      class="w-7 h-7 rounded-lg bg-gray-500 mx-1"
+                      onClick={() => setExpanded(false)}
+                    ></div>
+                  );
+                }
+              }
+            })}
+          </div>
+        </scrollview>
+      </div>
+    </div>
+  );
+};
