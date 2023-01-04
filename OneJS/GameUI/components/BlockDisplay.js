a43ce799b969bfcd0b539d0e6df5486a1ecb9067
איתי בר-חן <u9282364@bsmch.net>
2023-01-04 09:31:34 +0200

    add blocks

--- OneJS/GameUI/components/BlockDisplay.js
+++ OneJS/GameUI/components/BlockDisplay.js
@@ -5,18 +5,22 @@ var hooks_1 = require("preact/hooks");
 var BlockDisplay = function (_a) {
     var selectBlock = _a.selectBlock, blockObject = _a.blockObject, selected = _a.selected;
     var _b = (0, hooks_1.useState)(false), mouseDown = _b[0], setMouseDown = _b[1];
+    var _c = (0, hooks_1.useState)(false), showText = _c[0], setShowText = _c[1];
+    var blockTexture = blockObject.getImage();
     return ((0, preact_1.h)("div", { class: "rounded-2xl mx-[10px] mt-2 mb-2 transition-[all] ease-out duration-[0.25]", style: {
-            opacity: mouseDown ? 0.7 : 1,
+            opacity: selected ? 1 : 0.8,
             width: "20%",
-            height: "90px",
-            backgroundColor: selected ? "yellow" : "white",
+            height: "110px",
+            backgroundColor: "white",
         }, onClick: function () { return selectBlock(); }, onMouseDown: function () {
             setMouseDown(true);
+            setShowText(true);
         }, onMouseUp: function () {
             setMouseDown(false);
+            setShowText(false);
         } },
         (0, preact_1.h)("div", { class: "flex-col items-center justify-center w-full h-full" },
-            (0, preact_1.h)("image", { image: blockObject.getImage() }),
-            (0, preact_1.h)("div", { class: "text-2xl" }, blockObject.getGameObject().name))));
+            (0, preact_1.h)("div", { class: "scale-[10]" },
+                (0, preact_1.h)("image", { image: blockTexture })))));
 };
 exports.BlockDisplay = BlockDisplay;
--- OneJS/GameUI/components/BlockDisplay.tsx
+++ OneJS/GameUI/components/BlockDisplay.tsx
@@ -1,29 +1,37 @@
 import { h } from "preact";
 import { useState } from "preact/hooks";
+import { Texture } from "UnityEngine";
+import { Image } from "UnityEngine/UIElements";
 
 export const BlockDisplay = ({ selectBlock, blockObject, selected }) => {
   const [mouseDown, setMouseDown] = useState(false);
+  const [showText, setShowText] = useState(false);
+
+  const blockTexture = blockObject.getImage();
 
   return (
     <div
       class="rounded-2xl mx-[10px] mt-2 mb-2 transition-[all] ease-out duration-[0.25]"
       style={{
-        opacity: mouseDown ? 0.7 : 1,
+        opacity: selected ? 1 : 0.8,
         width: "20%",
-        height: "90px",
-        backgroundColor: selected ? "yellow" : "white",
+        height: "110px",
+        backgroundColor: "white",
       }}
       onClick={() => selectBlock()}
       onMouseDown={() => {
         setMouseDown(true);
+        setShowText(true);
       }}
       onMouseUp={() => {
         setMouseDown(false);
+        setShowText(false);
       }}
     >
       <div class="flex-col items-center justify-center w-full h-full">
-        <image image={blockObject.getImage()} />
-        <div class="text-2xl">{blockObject.getGameObject().name}</div>
+        <div class="scale-[10]">
+          <image image={blockTexture} />
+        </div>
       </div>
     </div>
   );
